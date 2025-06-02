f344db16447b412352d97e0ca985da9edd906e91
Ben Shephard <bshephard0@gmail.com>
2025-06-02 15:08:57 +1000

    fixed association responsiveness error

--- src/components/Associations/Associations.tsx
+++ src/components/Associations/Associations.tsx
@@ -1,40 +1,33 @@
-import { Container, Flex } from "@mantine/core";
+"use client"
+import { Box, Container, Grid } from "@mantine/core";
 import Image from 'next/image';
 
 
 export function Associations() {
   return (
-    <Container size="lg" py="xl">
-      <Flex 
-        pt={20}
-        pb={20}
-        justify="space-between"
-      >
-        <Image
-          height={100}
-          width={100}
-          alt="hia"
-          src="/HIA.jpeg"
-        />
-        <Image
-          height={100}
-          width={250}
-          alt="bpb"
-          src="/bpb.png"
-        />
-        <Image
-          height={100}
-          width={270}
-          alt="bpb"
-          src="/MBA.png"
-        />
-        <Image
-          height={100}
-          width={170}
-          alt="bpb"
-          src="/AIBS.png"
-        />
-      </Flex>
+    <Container size="lg" py="xl" my="md">
+      <Grid>
+        <Grid.Col span={{xs: 12, sm: 6, md: 4, lg: 3}}>
+          <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+            <Image height={100} width={250} alt="hia" src="/hia-80.png" />
+          </Box>
+        </Grid.Col>
+        <Grid.Col span={{xs: 12, sm: 6, md: 4, lg: 3}}>
+          <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+            <Image height={100} width={250} alt="bpb" src="/bpb.png" />
+          </Box>
+        </Grid.Col>
+        <Grid.Col span={{xs: 12, sm: 6, md: 4, lg: 3}}>
+          <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+            <Image height={100} width={270} alt="mba" src="/MBA.png" />
+          </Box>
+          </Grid.Col>
+        <Grid.Col span={{xs: 12, sm: 6, md: 4, lg: 3}}>
+          <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+            <Image height={100} width={170} alt="aibs" src="/AIBS.png" />
+          </Box>
+        </Grid.Col>
+      </Grid>
     </Container>
   )
 }
